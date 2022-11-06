c22306304a534c81969e0e83fa5e0f66bfa75de5
jooohyunpark <jhp527@nyu.edu>
2022-11-06 13:47:41 -0500

    add vuetify,

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,31 @@
 <template>
-  <Tutorial/>
+  <div>
+    <v-container>
+      <v-row>
+        <v-col cols="12" md="6" lg="3">
+          <div class="box"></div>
+        </v-col>
+        <v-col cols="12" md="6" lg="3">
+          <div class="box"></div>
+        </v-col>
+        <v-col cols="12" md="6" lg="3">
+          <div class="box"></div>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
-}
+  name: "IndexPage",
+};
 </script>
+
+<style lang="scss" scoped>
+.box {
+  width: 100%;
+  height: 50px;
+  background: gray;
+}
+</style>
---
+++ styles/vuetify.config.js
@@ -0,0 +1,11 @@
+export default {
+  breakpoint: {
+    thresholds: {
+      xs: 340,
+      sm: 540,
+      md: 800,
+      lg: 1280,
+      xl: 1600,
+    },
+  },
+};
